36580f05e75f35b8f83623c588773fece162df03
Vladislav Rozhdestvenskii <vrozhdestvog@gmail.com>
2026-02-16 22:45:21 +0300

    Добавлена сфера

---
+++ src/assets/BusinessCircle.jsx
@@ -0,0 +1,45 @@
+export const BusinessCircle = () => {
+  return (
+    <svg
+      width="292"
+      height="577"
+      viewBox="0 0 292 577"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_f_7599_2125)">
+        <ellipse
+          cx="355.116"
+          cy="288.481"
+          rx="72.4909"
+          ry="281.4"
+          transform="rotate(55.7202 355.116 288.481)"
+          fill="#0B3C45"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_f_7599_2125"
+          x="0"
+          y="0"
+          width="710.233"
+          height="576.962"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="BackgroundImageFix"
+            result="shape"
+          />
+          <feGaussianBlur
+            stdDeviation="59.5"
+            result="effect1_foregroundBlur_7599_2125"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
